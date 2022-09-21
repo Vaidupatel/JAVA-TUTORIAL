196dc3a6723447c70d865723fd30ee301c5551df
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-09-21 19:46:58 +0530

    Create hello.java

---
+++ JAVA PRACTICE/hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hello {
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String name;
+        System.out.println("hii!! What is your name? ");
+        name=s.next();
+        
+        System.out.println("hello "+name+" how are you?");
+
+    }
+}
