0fbaf37e49ebec7b9cd04837404da6c06e16a10b
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-09-21 22:56:48 +0530

    Create incdec.java

---
+++ JAVA PRACTICE/incdec.java
@@ -0,0 +1,14 @@
+class incdec
+{
+public static void main(String args[])
+{
+    //increment decrement operator
+    int i=75;
+    System.out.println(i++);    //first use i and than increment
+    System.out.println(i);
+    System.out.println(++i);    //first increment i and then use
+    System.out.println(i);
+
+}
+
+}
