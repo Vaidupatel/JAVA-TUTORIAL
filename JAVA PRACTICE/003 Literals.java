301d7e41e1716b5af7f7e5e2923b5c6dce1cdb88
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2022-11-29 18:14:14 +0530

    Create 003 Literals.java

---
+++ JAVA PRACTICE/003 Literals.java
@@ -0,0 +1,23 @@
+public class Literals {
+     public static void main(String[] args) {
+          byte age=34;
+          int age2=56;
+          short age3=83;
+          long age4=5666666666666666L;
+          char c='a';
+          float f1=5.6F;
+          double d=4.6;
+          boolean a=true;
+          String str="vaidik";
+          System.out.println(age);
+          System.out.println(age2);
+          System.out.println(age3);
+          System.out.println(age4);
+          System.out.println(c);
+          System.out.println(f1);
+          System.out.println(d);
+          System.out.println(a);
+          System.out.println(str);
+
+     }
+}
